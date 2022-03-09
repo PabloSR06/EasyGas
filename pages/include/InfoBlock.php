862d968b48c2609289ad6d3e5e31d1ab210c6b84
Pablo <47283527+PabloSR06@users.noreply.github.com>
2022-03-09 17:14:18 +0000

    print precio if it has

--- pages/include/InfoBlock.php
+++ pages/include/InfoBlock.php
@@ -1,24 +1,63 @@
 <?php
 //TODO PHPDOC
-function gasolinera($arraySimple){
+function gasolinera($array){
     //TODO Poner imagen
-    //FIX CSS
-    echo "
-    <a href='gasPage.php?valor=".base64_encode(serialize($arraySimple))."'>
+    //TODO CSS
+    $arraySimple = (array) $array;
+    
+    echo "<a href='gasPage.php?valor=".base64_encode(serialize($arraySimple))."'>";
+    ?>
         <div class='gasBorder'>
             <div class='gasInfoBlock'>
                 <img src='/Easygas/images/gasLogo/bp.png' alt='logo'>
                 <div class='gasNameBlock'>
-                    <h1>" . $arraySimple['Rótulo'] . "</h1>
+                    <h1> <?php echo $arraySimple['Rótulo'] ?></h1>
                 </div>
             </div>
             <div class='gasMoney'>
-                <p>Gasolina95: " . $arraySimple['Precio Gasolina 95 E5']  . "</p>
-                <p>Gasolina98: " . $arraySimple['Precio Gasolina 98 E5'] . "</p>
-                <p>Gasoleo: " . $arraySimple['Precio Gasoleo A'] . "</p>
-                <p>Gasoleo +: " . $arraySimple['Precio Gasoleo Premium'] . "</p>
+                
+                <?php 
+                    if($arraySimple['Precio Gasolina 95 E5'] == ""){
+                        if($arraySimple['Precio Gasolina 95 E10'] != ""){
+                            echo "<p>Gasolina95:". $arraySimple['Precio Gasolina 95 E10'] . "</p>";
+                        }
+                    }else{
+                        echo "<p>Gasolina95:". $arraySimple['Precio Gasolina 95 E5'] . "</p>";
+                    }
+                ?>
+                
+                
+                <?php 
+                    if($arraySimple['Precio Gasolina 98 E5'] == ""){
+                        if($arraySimple['Precio Gasolina 98 E10'] != ""){
+                            echo "<p>Gasolina98:". $arraySimple['Precio Gasolina 98 E10'] . "</p>";
+                        }
+                    }else{
+                        echo "<p>Gasolina98:". $arraySimple['Precio Gasolina 98 E5'] . "</p>";
+                    }
+                ?>
+                
+                
+                <?php 
+                    if($arraySimple['Precio Gasoleo A'] == ""){
+                        if($arraySimple['Precio Gasoleo B'] != ""){
+                            echo "<p>Gasoleo B:". $arraySimple['Precio Gasoleo B'] . "</p>";
+                        }
+                    }else{
+                        echo "<p>Gasoleo A:". $arraySimple['Precio Gasoleo A'] . "</p>";
+                    }
+                ?> 
+                <?php 
+                    if($arraySimple['Precio Gasoleo Premium'] != ""){
+                        
+                        echo "<p>Gasoleo +:". $arraySimple['Precio Gasoleo Premium'] . "</p>";
+                    }
+                ?>
+                
             </div>
         </div>
-    </a>";
+    </a>
+    <?php
+
 }
 ?>
